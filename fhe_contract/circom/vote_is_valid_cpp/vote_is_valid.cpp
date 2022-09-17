aeef2b872bc8395830e2d8dccf26739856a30c5e
Dev43 <patrick@bitbackapp.com>
2022-09-17 17:40:26 +0200

    regenerate artifacts

--- fhe_contract/circom/vote_is_valid_cpp/vote_is_valid.cpp
+++ fhe_contract/circom/vote_is_valid_cpp/vote_is_valid.cpp
@@ -11,15 +11,23 @@ void LessEqThan_2_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::stri
 void LessEqThan_2_run(uint ctx_index,Circom_CalcWit* ctx);
 void GreaterEqThan_3_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
 void GreaterEqThan_3_run(uint ctx_index,Circom_CalcWit* ctx);
-void vote_is_valid_4_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
-void vote_is_valid_4_run(uint ctx_index,Circom_CalcWit* ctx);
-Circom_TemplateFunction _functionTable[5] = { 
+void IsZero_4_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
+void IsZero_4_run(uint ctx_index,Circom_CalcWit* ctx);
+void ForceEqualIfEnabled_5_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
+void ForceEqualIfEnabled_5_run(uint ctx_index,Circom_CalcWit* ctx);
+void vote_is_valid_6_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
+void vote_is_valid_6_run(uint ctx_index,Circom_CalcWit* ctx);
+Circom_TemplateFunction _functionTable[7] = { 
 Num2Bits_0_run,
 LessThan_1_run,
 LessEqThan_2_run,
 GreaterEqThan_3_run,
-vote_is_valid_4_run };
-Circom_TemplateFunction _functionTableParallel[5] = { 
+IsZero_4_run,
+ForceEqualIfEnabled_5_run,
+vote_is_valid_6_run };
+Circom_TemplateFunction _functionTableParallel[7] = { 
+NULL,
+NULL,
 NULL,
 NULL,
 NULL,
@@ -29,13 +37,13 @@ uint get_main_input_signal_start() {return 2;}
 
 uint get_main_input_signal_no() {return 10;}
 
-uint get_total_signal_no() {return 50;}
+uint get_total_signal_no() {return 57;}
 
-uint get_number_of_components() {return 7;}
+uint get_number_of_components() {return 9;}
 
 uint get_size_of_input_hashmap() {return 256;}
 
-uint get_size_of_witness() {return 32;}
+uint get_size_of_witness() {return 33;}
 
 uint get_size_of_constants() {return 5;}
 
@@ -350,17 +358,129 @@ Fr_copy(aux_dest,&ctx->signalValues[ctx->componentMemory[mySubcomponents[0]].sig
 }
 }
 
-void vote_is_valid_4_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
+void IsZero_4_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
 ctx->componentMemory[coffset].templateId = 4;
+ctx->componentMemory[coffset].templateName = "IsZero";
+ctx->componentMemory[coffset].signalStart = soffset;
+ctx->componentMemory[coffset].inputCounter = 1;
+ctx->componentMemory[coffset].componentName = componentName;
+ctx->componentMemory[coffset].idFather = componentFather;
+ctx->componentMemory[coffset].subcomponents = new uint[0];
+}
+
+void IsZero_4_run(uint ctx_index,Circom_CalcWit* ctx){
+FrElement* signalValues = ctx->signalValues;
+u64 mySignalStart = ctx->componentMemory[ctx_index].signalStart;
+std::string myTemplateName = ctx->componentMemory[ctx_index].templateName;
+std::string myComponentName = ctx->componentMemory[ctx_index].componentName;
+u64 myFather = ctx->componentMemory[ctx_index].idFather;
+u64 myId = ctx_index;
+u32* mySubcomponents = ctx->componentMemory[ctx_index].subcomponents;
+bool* mySubcomponentsParallel = ctx->componentMemory[ctx_index].subcomponentsParallel;
+FrElement* circuitConstants = ctx->circuitConstants;
+std::string* listOfTemplateMessages = ctx->listOfTemplateMessages;
+FrElement expaux[4];
+FrElement lvar[0];
+uint sub_component_aux;
+Fr_neq(&expaux[0],&signalValues[mySignalStart + 1],&circuitConstants[1]); // line circom 30
+if(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 2];
+// load src
+Fr_div(&expaux[0],&circuitConstants[2],&signalValues[mySignalStart + 1]); // line circom 30
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+}else{
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 2];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
+}
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 0];
+// load src
+Fr_neg(&expaux[2],&signalValues[mySignalStart + 1]); // line circom 32
+Fr_mul(&expaux[1],&expaux[2],&signalValues[mySignalStart + 2]); // line circom 32
+Fr_add(&expaux[0],&expaux[1],&circuitConstants[2]); // line circom 32
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_mul(&expaux[1],&signalValues[mySignalStart + 1],&signalValues[mySignalStart + 0]); // line circom 33
+Fr_eq(&expaux[0],&expaux[1],&circuitConstants[1]); // line circom 33
+if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template/function " << myTemplateName << " line 33. " <<  "Followed trace of components: " << ctx->getTrace(myId) << std::endl;
+assert(Fr_isTrue(&expaux[0]));
+}
+
+void ForceEqualIfEnabled_5_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
+ctx->componentMemory[coffset].templateId = 5;
+ctx->componentMemory[coffset].templateName = "ForceEqualIfEnabled";
+ctx->componentMemory[coffset].signalStart = soffset;
+ctx->componentMemory[coffset].inputCounter = 3;
+ctx->componentMemory[coffset].componentName = componentName;
+ctx->componentMemory[coffset].idFather = componentFather;
+ctx->componentMemory[coffset].subcomponents = new uint[1];
+}
+
+void ForceEqualIfEnabled_5_run(uint ctx_index,Circom_CalcWit* ctx){
+FrElement* signalValues = ctx->signalValues;
+u64 mySignalStart = ctx->componentMemory[ctx_index].signalStart;
+std::string myTemplateName = ctx->componentMemory[ctx_index].templateName;
+std::string myComponentName = ctx->componentMemory[ctx_index].componentName;
+u64 myFather = ctx->componentMemory[ctx_index].idFather;
+u64 myId = ctx_index;
+u32* mySubcomponents = ctx->componentMemory[ctx_index].subcomponents;
+bool* mySubcomponentsParallel = ctx->componentMemory[ctx_index].subcomponentsParallel;
+FrElement* circuitConstants = ctx->circuitConstants;
+std::string* listOfTemplateMessages = ctx->listOfTemplateMessages;
+FrElement expaux[6];
+FrElement lvar[0];
+uint sub_component_aux;
+{
+uint aux_create = 0;
+int aux_cmp_num = 0+ctx_index+1;
+uint csoffset = mySignalStart+3;
+for (uint i = 0; i < 1; i++) {
+std::string new_cmp_name = "isz";
+IsZero_4_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
+mySubcomponents[aux_create+i] = aux_cmp_num;
+csoffset += 3 ;
+aux_cmp_num += 1;
+}
+}
+{
+uint cmp_index_ref = 0;
+{
+PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 1];
+// load src
+Fr_sub(&expaux[0],&signalValues[mySignalStart + 2],&signalValues[mySignalStart + 1]); // line circom 54
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+// need to run sub component
+assert(!(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter));
+IsZero_4_run(mySubcomponents[cmp_index_ref],ctx);
+}
+Fr_sub(&expaux[2],&circuitConstants[2],&ctx->signalValues[ctx->componentMemory[mySubcomponents[0]].signalStart + 0]); // line circom 56
+Fr_mul(&expaux[1],&expaux[2],&signalValues[mySignalStart + 0]); // line circom 56
+Fr_eq(&expaux[0],&expaux[1],&circuitConstants[1]); // line circom 56
+if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template/function " << myTemplateName << " line 56. " <<  "Followed trace of components: " << ctx->getTrace(myId) << std::endl;
+assert(Fr_isTrue(&expaux[0]));
+}
+
+void vote_is_valid_6_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
+ctx->componentMemory[coffset].templateId = 6;
 ctx->componentMemory[coffset].templateName = "vote_is_valid";
 ctx->componentMemory[coffset].signalStart = soffset;
 ctx->componentMemory[coffset].inputCounter = 10;
 ctx->componentMemory[coffset].componentName = componentName;
 ctx->componentMemory[coffset].idFather = componentFather;
-ctx->componentMemory[coffset].subcomponents = new uint[2];
+ctx->componentMemory[coffset].subcomponents = new uint[3];
 }
 
-void vote_is_valid_4_run(uint ctx_index,Circom_CalcWit* ctx){
+void vote_is_valid_6_run(uint ctx_index,Circom_CalcWit* ctx){
 FrElement* signalValues = ctx->signalValues;
 u64 mySignalStart = ctx->componentMemory[ctx_index].signalStart;
 std::string myTemplateName = ctx->componentMemory[ctx_index].templateName;
@@ -376,8 +496,8 @@ FrElement lvar[2];
 uint sub_component_aux;
 {
 uint aux_create = 0;
-int aux_cmp_num = 0+ctx_index+1;
-uint csoffset = mySignalStart+13;
+int aux_cmp_num = 2+ctx_index+1;
+uint csoffset = mySignalStart+20;
 for (uint i = 0; i < 1; i++) {
 std::string new_cmp_name = "is_less_than";
 LessEqThan_2_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
@@ -388,14 +508,26 @@ aux_cmp_num += 3;
 }
 {
 uint aux_create = 1;
-int aux_cmp_num = 3+ctx_index+1;
-uint csoffset = mySignalStart+31;
+int aux_cmp_num = 5+ctx_index+1;
+uint csoffset = mySignalStart+38;
 for (uint i = 0; i < 1; i++) {
 std::string new_cmp_name = "is_more_than";
 GreaterEqThan_3_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
 mySubcomponents[aux_create+i] = aux_cmp_num;
 csoffset += 18 ;
 aux_cmp_num += 3;
+}
+}
+{
+uint aux_create = 2;
+int aux_cmp_num = 0+ctx_index+1;
+uint csoffset = mySignalStart+14;
+for (uint i = 0; i < 1; i++) {
+std::string new_cmp_name = "f_equal";
+ForceEqualIfEnabled_5_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
+mySubcomponents[aux_create+i] = aux_cmp_num;
+csoffset += 6 ;
+aux_cmp_num += 2;
 }
 }
 {
@@ -515,16 +647,71 @@ delete [] temp;
 printf("\n");
 }
 {
+uint cmp_index_ref = 2;
+{
+PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 0];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[2]);
+}
+// no need to run sub component
+assert(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter);
+}
+{
+uint cmp_index_ref = 2;
+{
+PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 1];
+// load src
+Fr_mul(&expaux[0],&signalValues[mySignalStart + 11],&signalValues[mySignalStart + 12]); // line circom 34
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+// no need to run sub component
+assert(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter);
+}
+{
+uint cmp_index_ref = 2;
+{
+PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 2];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[2]);
+}
+// need to run sub component
+assert(!(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter));
+ForceEqualIfEnabled_5_run(mySubcomponents[cmp_index_ref],ctx);
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 13];
+// load src
+// end load src
+Fr_copy(aux_dest,&ctx->signalValues[ctx->componentMemory[mySubcomponents[1]].signalStart + 0]);
+}
+{
+printf("The result to forced equal is ");
+}
+{
+printf(" ");
+}
+{
+char* temp = Fr_element2str(&signalValues[mySignalStart + 13]);
+printf("%s",temp);
+delete [] temp;
+}
+{
+printf("\n");
+}
+{
 PFrElement aux_dest = &signalValues[mySignalStart + 0];
 // load src
-Fr_mul(&expaux[0],&signalValues[mySignalStart + 11],&signalValues[mySignalStart + 12]); // line circom 32
+Fr_mul(&expaux[0],&signalValues[mySignalStart + 11],&signalValues[mySignalStart + 12]); // line circom 42
 // end load src
 Fr_copy(aux_dest,&expaux[0]);
 }
 }
 
 void run(Circom_CalcWit* ctx){
-vote_is_valid_4_create(1,0,ctx,"main",0);
-vote_is_valid_4_run(0,ctx);
+vote_is_valid_6_create(1,0,ctx,"main",0);
+vote_is_valid_6_run(0,ctx);
 }
 
